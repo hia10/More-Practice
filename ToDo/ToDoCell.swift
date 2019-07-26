24e556f50d21274a2763d51aff9285d8b202d4c7
Hisham Abi Ammar <hisham.abi.ammar@gmail.com>
2019-07-26 20:30:15 +0300

    Initial Commit

--- ToDo/ToDoCell.swift
+++ ToDo/ToDoCell.swift
@@ -35,10 +35,10 @@ class ToDoCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+        
+        print ("Hello World")
 
     }
-
-    
     
     
 }
